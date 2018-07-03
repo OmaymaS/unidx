3b8cc185cf4e9ab8327c2b925fd36fb1d3d25210
omaymaS <omayma.saeed@gmail.com>
2018-07-03 15:30:08 +0200

    add hdi dataset doc

---
+++ R/hdi.R
@@ -0,0 +1,17 @@
+#' hdi
+#' Human Development Index data by UNDP for 188 country from 1990 to 2015.
+#'
+#' @format The dataframe `hdi` has 4888 rows and 3 variables
+#'
+#' - **country**: factor with 188 levels
+#' - **year**: years between 1900 - 2015
+#' - **human_development_index**: measured index with lower values reflecting lower development
+#'
+#' The data originally comes in wide format available in `hdi_wide` dataframe
+#'
+#' @source
+#' - [UNDP](http://hdr.undp.org/en/data)
+#'
+#' @md
+
+"hdi"
