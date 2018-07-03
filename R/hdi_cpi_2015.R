f4008b852a0e06368d90a6918bfe1f1322e43c65
omaymaS <omayma.saeed@gmail.com>
2018-07-03 14:29:10 +0200

    modify hdi_cpi_2015 doc
    replace \item with markdown items

--- R/hdi_cpi_2015.R
+++ R/hdi_cpi_2015.R
@@ -1,21 +1,20 @@
-#' hdi_cpi_2015 Data
+#' hdi_cpi_2015
 #'
 #' A dataframe combining Human Development Index by UNDP and Corruption Perception Index from Transparency International for 121 countries.
 #'
 #' @format  The main dataframe `hdi_cpi_2015` has 121 rows and 10 variables:
-#' \describe{
-#' \item{country}{factor with 121 levels}
-#' \item{country_code}{factor}
-#' \item{region}{factor with 6 levels}
-#' \item{continent}{factor with 5 levels}
-#' \item{population}{country population in 2015}
-#' \item{corruption_perception_index}{lower values reflect higher perceived corruption}
-#' \item{cpi_rank}{country rank according to corruption perception index}
-#' \item{human_development_index}{lower values reflect lowerdevelopment}
-#' \item{hdi_rank}{country rank according to human development index}
-#' \item{year}{year 2015}
-#' }
-#' @md
+
+#' - **country**: factor with 121 levels
+#' - **country_code**: factor
+#' - **region**: factor with 6 levels
+#' - **continent**: factor with 5 levels
+#' - **population**: country population in 2015
+#' - **corruption_perception_index**:  lower values reflect higher perceived corruption
+#' - **cpi_rank**:  country rank according to corruption perception index
+#' - **human_development_index**:  lower values reflect lowerdevelopment
+#' - **hdi_rank**:  country rank according to human development index
+#' - **year**:  year 2015
+#'
 #' @source
 #' - [UNDP](http://hdr.undp.org/en/data)
 #' - [Transparency.org](https://www.transparency.org/cpi2015#downloads)
